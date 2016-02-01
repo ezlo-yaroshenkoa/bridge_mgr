22d2dec3d059390f4a933314875aaacfc160e1e5
oleksii <oleksii.iaroshenko@gmail.com>
2016-02-01 04:27:35 -0800

    rest logic

--- bridge_mgr_rest/bridge_mgr_rest/controllers/root.py
+++ bridge_mgr_rest/bridge_mgr_rest/controllers/root.py
@@ -1,22 +1,14 @@
 from pecan import expose, redirect
-from webob.exc import status_map
-
 
 class RootController(object):
+    @expose(method='POST')
+    def create_bridge(self, bridge_name):
+        return 'create bridge %s' % bridge_name
 
-    @expose(generic=True, template='index.html')
-    def index(self):
-        return dict()
+    @expose(method='POST')
+    def remove_bridge(self, bridge_name):
+        return 'remove bridge %s' % bridge_name
 
-    @index.when(method='POST')
-    def index_post(self, q):
-        redirect('http://pecan.readthedocs.org/en/latest/search.html?q=%s' % q)
-
-    @expose('error.html')
-    def error(self, status):
-        try:
-            status = int(status)
-        except ValueError:  # pragma: no cover
-            status = 500
-        message = getattr(status_map.get(status), 'explanation', '')
-        return dict(status=status, message=message)
+    @expose(method='GET')
+    def get_bridges(self):
+        return 'get bridges'
