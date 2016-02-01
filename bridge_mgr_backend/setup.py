cb15fe4adf91ae76ebf7bbb7e9c4e1d3b0dc6359
oleksii <oleksii.iaroshenko@gmail.com>
2016-02-01 02:11:34 -0800

    pecan rest added

---
+++ bridge_mgr_backend/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='bridge_mgr_backend',
+      version='0.1',
+      description='bridge manager backend',
+      url='https://github.com/oleksii-iaroshenko/bridge_mgr',
+      author='okeksii iaroshenko',
+      author_email='oleksii.iaroshenko@gmail.com',
+      license='',
+      packages=['bridge_mgr_backend'],
+      install_requires=[
+          '',
+      ],
+      zip_safe=False)
