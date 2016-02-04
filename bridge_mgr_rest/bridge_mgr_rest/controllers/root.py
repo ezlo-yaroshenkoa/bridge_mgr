a0e567e9a4c3539dd55bdd37ff814d8d27b90f96
oleksii <oleksii.iaroshenko@gmail.com>
2016-02-04 05:31:17 -0800

    rest fix

--- bridge_mgr_rest/bridge_mgr_rest/controllers/root.py
+++ bridge_mgr_rest/bridge_mgr_rest/controllers/root.py
@@ -1,34 +1,41 @@
 from pecan import expose, abort
 from bridges_db import bridges_db
 from bridge_mgr_rpc_client import BridgeManagerRpcClient
+import json
 
 class RootController(object):
     def __init__(self):
         self.bridges_db_ = bridges_db()
         self.bridge_mgr_rpc_client_ = BridgeManagerRpcClient()
 
-    @expose('json')
+    @expose(generic=True)
     def bridges(self):
         return self.bridges_db_.get_bridges()
 
     @bridges.when(method='POST')
     def create_bridge(self, bridge_name):
-        data = "{'action':1,'bridge_name':'{0}'}".format(bridge_name)
+        data = {}
+        data['action'] = 1
+        data['bridge_name'] = bridge_name
+        json_data = json.dumps(data)
 
-        if self.bridge_mgr_rpc_client_.send_data(data):
+        if self.bridge_mgr_rpc_client_.send_data(json_data):
             self.bridges_db_.add_bridge(bridge_name)
         else:
             abort(500)
 
-        return 'create bridge %s' % bridge_name
+        return 'bridge {0} added'.format(bridge_name)
 
     @bridges.when(method='DELETE')
     def remove_bridge(self, bridge_name):
-        data = "{'action':0,'bridge_name':'{0}'}".format(bridge_name)
+        data = {}
+        data['action'] = 0
+        data['bridge_name'] = bridge_name
+        json_data = json.dumps(data)
 
-        if self.bridge_mgr_rpc_client_.send_data(data):
+        if self.bridge_mgr_rpc_client_.send_data(json_data):
             self.bridges_db_.del_bridge(bridge_name)
         else:
             abort(500)
 
-        return 'remove bridge %s' % bridge_name
+        return 'bridge {0} deleted'.format(bridge_name)
